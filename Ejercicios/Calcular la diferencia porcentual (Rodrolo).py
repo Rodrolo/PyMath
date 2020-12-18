a347dbfbfd049bf3fb875979b5b0eb14b8e7abba
Rodrolo <72154169+Rodrolo@users.noreply.github.com>
2020-12-18 15:07:04 +0100

    Calcular la diferencia porcentual (Rodrolo).

--- Ejercicios/Calcular la diferencia porcentual (Rodrolo).py
+++ Ejercicios/Calcular la diferencia porcentual (Rodrolo).py
@@ -4,7 +4,7 @@ print("<==Calcular la diferencia porcentual.==>")
 #Variables
 x = int(input("Valor nuevo: ")) #Campo para ingresar variable.
 y = int(input("Valor antiguo: ")) #Campo para ingresar variable.
-p1 = x - y #Valor antiguo menos valor nuevo (4000 - 3600).
+p1 = x - y #Valor nuevo menos valor antiguo (4000 - 3600).
 p2 = p1 / y #Var. P1 partido del valor antiguo.
 r = p2 * 100 #Var. P2 multiplicada por 100.
 
