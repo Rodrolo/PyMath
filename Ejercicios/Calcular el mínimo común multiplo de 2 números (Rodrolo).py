a34452a7914727615552b5ef538da7fe6a25f888
Rodrolo <72154169+Rodrolo@users.noreply.github.com>
2021-01-21 16:31:54 +0100

    Calcular el mínimo común multiplo de 2 números (Rodrolo).
    Calcular el mínimo común multiplo de 2 números (Rodrolo).

--- Ejercicios/Calcular el mínimo común multiplo de 2 números (Rodrolo).py
+++ Ejercicios/Calcular el mínimo común multiplo de 2 números (Rodrolo).py
@@ -7,12 +7,12 @@ y = int(input("Número Y: "))
 #Definimos la función "mcm" con nuestras dos variables: "x" e "y".
 def mcm(x, y):
 
-    if x < y:
+    if x < y: #Si "x" es menor que "y", entonces "z" es "x". Si no, lo contrario.
         z = x
     else:
         z = y
 
-    while not (x % z == 0 and y % z == 0):
+    while not (x % z == 0 and y % z == 0): #Modulo (%): resto de división.
         z -= 1
     else:
         return z
